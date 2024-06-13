460bf8df96d76c5b520fd2fcd621d6a4e7bc8ff1
Mathias Sundman <mathias.sundman@it-total.se>
2024-06-13 11:22:42 +0200

    Added QR generator

--- src/app/qr/qr.component.ts
+++ src/app/qr/qr.component.ts
@@ -1,11 +1,22 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { QRCodeModule } from 'angularx-qrcode';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @Component({
   selector: 'app-qr',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    MatButtonModule,
+    QRCodeModule
+  ],
   template: `
-  <p>qr works!</p>
+  <h2>Postnord</h2>
+  <qrcode [qrdata]="'Your data string'" [width]="256" [errorCorrectionLevel]="'M'"></qrcode>
+  <h2>5 kg</h2>
+  <button mat-raised-button color="primary" [routerLink]="['/couriers']">Stäng</button>
   `,
   styleUrls: ['./qr.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
